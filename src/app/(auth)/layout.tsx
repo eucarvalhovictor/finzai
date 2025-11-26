d70031384166e9cb141aafb7a174ca07575646a6
zeephx <victorcarvalhocarvalho4@gmail.com>
2025-11-26 18:26:31 +0000

    deixe a tela de login e registro em 100vh e 100vw, deixando ela tela che

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -7,12 +7,12 @@ export default function AuthLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex min-h-screen flex-col items-center sm:justify-center p-4 relative">
+    <div className="flex min-h-screen flex-col items-center justify-center relative">
       <DotsBackground />
-      <div className="mb-8 z-10 pt-16 sm:pt-0">
+      <div className="absolute top-16 sm:relative sm:top-auto sm:mb-8 z-10">
         <AppLogo />
       </div>
-      <div className="z-10 w-full">
+      <div className="z-10 w-full p-4">
         {children}
       </div>
     </div>
