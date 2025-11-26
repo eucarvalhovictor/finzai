e56db204be0492d83f8a594642821cb635b1f2bd
zeephx <victorcarvalhocarvalho4@gmail.com>
2025-11-26 17:38:44 +0000

    Quero que você ajuste o tempo de delay do projeto. Todas as vezes que cl

--- src/app/(app)/admin/layout.tsx
+++ src/app/(app)/admin/layout.tsx
@@ -86,7 +86,7 @@ export default function AdminLayout({
             </Link>
         ))}
       </nav>
-      <div className="animate-fade-in">
+      <div>
         {children}
       </div>
     </div>
